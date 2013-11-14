ed35a1f225812539a99614e91de53e771afacc08
johnny1212 <johnny1212@volny.cz>
2013-11-14 20:53:06 +0100

    tvorba - Lineární seznam

--- Queue/src/Queue/Queue.java
+++ Queue/src/Queue/Queue.java
@@ -1,9 +1,29 @@
 package Queue;
 
+import LinearniSeznam.LinearniSeznam;
+
 /**
  *
  * @author Johnny
  */
-public class Queue {
-    
+public class Queue<T> {
+
+    private LinearniSeznam seznam = new LinearniSeznam();
+    private int count = 0;
+
+    public Queue() {
+    }
+
+    public void push(T data) {
+        seznam.addNodeLast(data);
+        count++;
+    }
+
+    public T pop() {
+        if (count == 0) {
+            return null;
+        }
+        count--;
+        return (T) seznam.deleteFirst();
+    }
 }
