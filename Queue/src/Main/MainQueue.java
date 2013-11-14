fdc4329340ef630296bebad2c56b941965fc5ccb
johnny1212 <johnny1212@volny.cz>
2013-11-14 20:52:17 +0100

    tvorba - Lineární seznam

--- Queue/src/Main/MainQueue.java
+++ Queue/src/Main/MainQueue.java
@@ -1,5 +1,7 @@
 package Main;
 
+import Queue.Queue;
+
 /**
  *
  * @author Johnny
@@ -7,6 +9,15 @@ package Main;
 public class MainQueue {
 
     public static void main(String[] args) {
-        System.out.println("AA");
+        System.out.println("FRONTA");
+
+        Queue<String> fronta = new Queue();
+        fronta.push("AA");
+        fronta.push("BB");
+        fronta.push("CC");
+
+        System.out.println("-" + fronta.pop());
+        System.out.println("-" + fronta.pop());
+        System.out.println("-" + fronta.pop());
     }
 }
