81029910e2f6144d8c6453628ac50f2480169db9
johnny1212 <johnny1212@volny.cz>
2013-11-14 20:45:32 +0100

    tvorba - Lineární seznam

--- Queue/src/Main/Main.java
+++ Queue/src/Main/Main.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
 /**
@@ -10,9 +6,6 @@ package Main;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("AA");
     }
